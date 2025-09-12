f0054065cab3dcf23d214bfe3bee6709e3a5e42d
vriti62 <jaitleyvriti@gmail.com>
2025-09-13 03:26:16 +0530

    added redis url

--- utils/redis.js
+++ utils/redis.js
@@ -1,7 +1,8 @@
 // redis.js
 const Redis = require("ioredis");
 
+
 // default localhost:6379
-const redis = new Redis();
+const redis = new Redis(process.env.REDIS_URL); 
 
 module.exports = redis;
