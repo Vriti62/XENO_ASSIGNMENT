231fe5f8b5cf396d9d40cb4d9402bcd01c872fb8
vriti62 <jaitleyvriti@gmail.com>
2025-09-13 15:12:36 +0530

    fixed-some-files

--- Controllers/webhook.controller.js
+++ Controllers/webhook.controller.js
@@ -62,11 +62,6 @@ exports.webhookData = async (req, res) => {
           data:inventory_items,
           msg:"inventory_items created successfully!"
         })
-      case "checkout/abandon":
-        const checkout = await handleCheckoutWebhook(data,storeData);
-        return res.status(200).json({
-          data:checkout
-        })
 
       default:
         return res.status(400).json({ msg: "Unhandled Shopify event" });
@@ -164,11 +159,10 @@ async function removeDeletedInDB(data,storeData){
       id: data.id.toString()
     }
   })
-  return deletedOrder;
 }
 
 async function saveInventoryItemToDB(data) {
-  const inventory_items = await prisma.inventoryItem.create({
+  return await prisma.inventoryItem.create({
     data: {
       id: data.id.toString(),
       sku: data.sku,
@@ -177,40 +171,6 @@ async function saveInventoryItemToDB(data) {
       weight_unit: data.weight_unit
     }
   });
-  return inventory_items;
-}
-
-async function handleCheckoutWebhook(req, res) {
-  try {
-    const { id, email, line_items } = req.body; // Shopify payload
-
-    if (!email) {
-      return res.status(400).json({ success: false, msg: "Email required" });
-    }
-
-    // Redis key banayenge
-    const checkoutKey = `checkout:${id}`;
-    await redis.setex(checkoutKey, 180, JSON.stringify({ email, line_items }));
-
-    // Timer lagayenge (3 min demo ke liye)
-    setTimeout(async () => {
-      const data = await redis.get(checkoutKey);
-      if (data) {
-        const { email, line_items } = JSON.parse(data);
-
-        // Abandoned cart email
-        await abandonCartEmail(email, line_items);
-
-        // Redis key delete
-        await redis.del(checkoutKey);
-      }
-    }, 180000);
-
-    res.status(200).json({ success: true, msg: "Checkout tracked" });
-  } catch (err) {
-    console.error("❌ Checkout webhook error:", err);
-    res.status(500).json({ success: false, msg: err.message });
-  }
 }
 
 
