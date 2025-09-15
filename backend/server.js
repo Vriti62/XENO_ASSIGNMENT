c0346322521b4fe28ca68811319001559deb9a25
vriti62 <jaitleyvriti@gmail.com>
2025-09-15 09:55:12 +0530

    removed-cors

--- backend/server.js
+++ backend/server.js
@@ -1,38 +1,41 @@
-const dotenv = require('dotenv');
+const dotenv = require("dotenv");
 dotenv.config();
-const express = require('express');
+const express = require("express");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const prisma = require("./db/db.config");
+
+const webhookData = require("./Routes/webhook.routes");
+const dataRoutes = require("./Routes/data.routes");
+const userRoutes = require("./Routes/user.routes");
+
 const app = express();
 const PORT = process.env.PORT || 5000;
-const cookieParser = require('cookie-parser');
-const prisma = require('./db/db.config');
-const  webhookData  = require('./Routes/webhook.routes');
-const  dataRoutes = require('./Routes/data.routes');
-const userRoutes = require('./Routes/user.routes');
-
-
-const cors = require('cors');
-
-app.use(cors({
-  origin: 'https://xeno-assignment-2f.onrender.com',
-  credentials: true,
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
-}));
 
 
 app.use(cookieParser());
-app.use(express.urlencoded({extended: true}));
-app.use(express.json({
-  verify: (req, res, buf) => {
-    req.rawBody = buf;
-  }
-}));
+app.use(express.urlencoded({ extended: true }));
+app.use(
+  express.json({
+    verify: (req, res, buf) => {
+      req.rawBody = buf;
+    },
+  })
+);
+
 
 app.use("/api", webhookData);
-app.use('/api/user', userRoutes);
-app.use("/api/data",  dataRoutes);
+app.use("/api/user", userRoutes);
+app.use("/api/data", dataRoutes);
 
 
-app.listen(PORT, ()=>{
-    console.log(`Server is running on port ${PORT}`);
-})
+app.use(express.static(path.join(__dirname, "build")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
+});
+
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
