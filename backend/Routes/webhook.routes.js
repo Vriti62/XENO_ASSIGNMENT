fe54ea1c9b6e750aec593277d5f0b98bac825db8
vriti62 <jaitleyvriti@gmail.com>
2025-09-15 22:49:04 +0530

    updated-controller-to-fix-bugs

--- backend/Routes/webhook.routes.js
+++ backend/Routes/webhook.routes.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const { webhookData } = require('../Controllers/webhook.controller');
-const {verifyShopifyWebhook} = require('../middleware/auth_hmac')
+const {verifyShopifyWebhook} = require('../middleware/auth_hmac');
+const { onboardStore } = require('../Controllers/store.controller');
 const router = express.Router();
 
 router.post("/webhook", verifyShopifyWebhook, webhookData);
+router.post("/store", onboardStore)
 
 module.exports = router;
---
+++ backend/prisma/migrations/20250915171523_add_webhook_secret_to_store/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE "public"."Store" ADD COLUMN     "webhook_secret" TEXT;
